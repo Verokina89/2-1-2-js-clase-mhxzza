9f695f06705d8dba8f4afc55af1c3a1e04f3e45e
Verokina89 <v.isabel1989@gmail.com>
2024-08-06 20:56:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2-1-2-js-clase-mhxzza
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Verokina89/2-1-2-js-clase-mhxzza)
---
+++ script.js
@@ -0,0 +1,27 @@
+//Declara dos variables numéricas y realiza operaciones aritméticas con ellas (suma, resta, multiplicación, división, módulo) y muestra los resultados en la consola.
+let numeroUno = 8;
+const numeroDos = 4;
+//AQUI VAMOS A REALIZAR COMPARACIONES.
+let textoUno = "10"
+
+// PARA ASIGNAR Y DECLARAR SE PUEDE TRABAJAR CON AMBOS (LET O CONST) //
+console.log(numeroUno + numeroDos);
+console. log (numeroUno - numeroDos);
+console. log(numeroUno * numeroDos);
+console. log(numeroUno / numeroDos);
+console. log(numeroUno % numeroDos);
+//AQUI VAMOS A REALIZAR COMPARACIONES.
+console.log(numeroUno == textoUno);
+
+//Realiza operaciones de comparación entre las variables y muestra los resultados en la consola.
+console.log(numeroUno == textoUno);
+
+console.log(numeroUno < numeroDos);
+console.log(numeroUno > numeroDos);
+console.log(numeroUno <= numeroDos);
+console.log(numeroUno != numeroDos);
+
+//Utiliza operadores lógicos para combinar condiciones y muestra los resultados en la consola.
+console.log(numeroUno && numeroDos);
+
+
